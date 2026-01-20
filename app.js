3e3e4438ddb2fb335065c6d2ed9dfa2f0e8372ea
jjin542 <jjin542@gmail.com>
2026-01-20 10:34:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import { createClient } from "https://ibjjbgthwmpvifbzxhwa.supabase.co";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "여기에_URL";
+const SUPABASE_URL = "https://ibjjbgthwmpvifbzxhwa.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImliampiZ3Rod21wdmlmYnp4aHdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4NjExMTcsImV4cCI6MjA4NDQzNzExN30.Bb3fyGlJ_16gao6W8P0yaMotsD5DIEeTJVan3m5OKQw
 ";
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
